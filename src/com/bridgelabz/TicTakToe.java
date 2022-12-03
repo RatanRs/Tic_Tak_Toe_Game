988e8b01b1462949e1c7c1a2438eab9cb6821cb1
9663461085 <ratansollapure16310@gmail.com>
2022-12-03 18:36:06 +0530

    Added:Displayed Game board to user

--- src/com/bridgelabz/TicTakToe.java
+++ src/com/bridgelabz/TicTakToe.java
@@ -11,13 +11,10 @@ public class TicTakToe {
 	static char board[] = new char[10];
 	static char player;
 	static char computer;
-	/***
-	 * Used Scanner Class to take input from user.
-	 */
-	Scanner input = new Scanner(System.in);
+	Scanner scanner = new Scanner(System.in);
 
 	/*
-	 * Created method called result
+	 * Created method called Result
 	 */
 	public void result() {
 		for (int i = 1; i < 10; i++) {
@@ -25,12 +22,23 @@ public class TicTakToe {
 		}
 	}
 
+	/*
+	 * Method to show Game Board.
+	 */
+	public void showBoard() {
+		System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+		System.out.println("---------");
+		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+		System.out.println("---------");
+		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+	}
+
+	/*
+	 * Logic for checking Player selected letter
+	 */
 	public void checkResult() {
 		System.out.println("please select your choice letter: 'X' or 'Y' ");
-		char choice = input.next().charAt(0);
-		/*
-		 * Used If else condition to check the condition 
-		 */
+		char choice = scanner.next().toUpperCase().charAt(0);
 		if (choice == 'X') {
 			System.out.println("Player selected letter is:" + 'X');
 			System.out.println("Computer selected letter is:" + 'O');
@@ -42,20 +50,19 @@ public class TicTakToe {
 	}
 
 	/**
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		System.out.println("Welcom to the Tic Tac Toe Game program");
-		System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
-		System.out.println("---------");
-		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
-		System.out.println("---------");
-		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+
 		/*
-		 * Created object of class
-		 * 
+		 * Creating object of class
 		 */
-		TicTakToe game = new TicTakToe();
-		game.checkResult();
+		TicTakToe tictactoe = new TicTakToe();
+		tictactoe.result();
+		tictactoe.showBoard();
+		tictactoe.checkResult();
 	}
+
 }
