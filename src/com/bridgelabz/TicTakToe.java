5639fccb3cc4f0e13d0536d3c3863f0af7c19cef
9663461085 <ratansollapure16310@gmail.com>
2022-12-03 18:13:58 +0530

    Added:Welcome Meassge to TicTakToe game

---
+++ src/com/bridgelabz/TicTakToe.java
@@ -0,0 +1,32 @@
+package com.bridgelabz;
+/***
+ * 
+ * @author Admin
+ *
+ */
+public class TicTakToe {
+static char board[] = new char[10];
+	static char player;
+	static char computer;
+	/***
+	 * Created method 
+	 */
+    public void result() {
+		for (int i = 1; i < 10; i++) {
+			board[i] = ' ';
+		}
+	}
+
+	
+	public static void main(String[] args) {
+		System.out.println("Welcom to the Tic Tac Toe Game program");
+		System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+		System.out.println("---------");
+		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+		System.out.println("---------");
+		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+
+	}
+
+}
+
